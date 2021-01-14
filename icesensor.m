62865c1940c41ffc0e808600cff04530488f1470
chxin <chengxinhust@163.com>
2021-01-14 18:50:39 +0800

    Add Feature: save matlab message to file

--- icesensor.m
+++ icesensor.m
@@ -73,7 +73,7 @@ ylabel('冰厚')
 string_2 = {'二维PCASVR模型测试集预测结果对比';
            ['mse = ' num2str(error_1test(2)) ' R^2 = ' num2str(error_1test(3))]};
 title(string_2)
-saveas(gcf, '3-2d-PCASVR', 'png')
+saveas(gcf, './build/3-2d-PCASVR', 'png')
 %% 三维主成分分析
 testpridict=test;
 testpridict(:,3)=Predict_1test;
@@ -113,7 +113,7 @@ xlabel('v1')
 ylabel('v2')
 zlabel('冰厚')
 grid on;
-saveas(gcf, '2-3d-ice-thickness', 'png')
+saveas(gcf, './build/2-3d-ice-thickness', 'png')
 %% 数据归一化
 % 输入电压
 [vol_train,inputps] = mapminmax(vol_train');%对电压值的两个特征都分别行归一化处理
@@ -164,7 +164,7 @@ xlabel('样本编号')
 ylabel('冰厚')
 string_2 = {'三维PCASVR模型测试集预测结果'};
 title(string_2)
-saveas(gcf, '4-3d-PCASVR-predict', 'png')
+saveas(gcf, './build/4-3d-PCASVR-predict', 'png')
 figure(5)
 plot(1:length(test(:,3)),test(:,3),'r-*',1:length(test(:,3)),Predict_2test,'b:o');
 grid on
@@ -173,4 +173,4 @@ xlabel('样本编号')
 ylabel('冰厚')
 string_2 = {'三维PCASVR模型测试集预测结果'};
 title(string_2)
-saveas(gcf, '5-3d-PCASVR-origin', 'png')
+saveas(gcf, './build/5-3d-PCASVR-origin', 'png')
